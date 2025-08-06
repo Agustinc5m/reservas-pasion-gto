856f07855431b38bb91230c9399400f4b5a73126
Raul Muro <agustinc5m@gmail.com>
2025-08-06 16:05:43 -0600

    Update App component title and remove count button

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -16,11 +16,8 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+    <h1>Pasion Guanajuato</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
